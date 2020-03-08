6127516331c42a8ebbba157e9fddf02b6d429f69
Daniel Lennertz <danielelennertz@gmail.com>
2020-03-08 19:25:07 -0300

    c1 - adicionando espaços apos ':'

--- ex6_ler.cpp
+++ ex6_ler.cpp
@@ -6,9 +6,9 @@ using namespace std;
 //l representa linha; c representa coluna; x é a variavel string
 
 void TMatriz::ler(){
-	cout << "Digite o numero de linhas:";
+	cout << "Digite o numero de linhas :";
 	cin >> qtdl;
-	cout << "Digite o numero de colunas:";
+	cout << "Digite o numero de colunas :";
 	cin >> qtdc;
 	for(int l=0;l<qtdl;l++)
 	{
